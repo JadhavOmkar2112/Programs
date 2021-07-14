4ca4a0b84255ae9e5847a3280d912c42244c24af
JadhavOmkar2112 <86395951+JadhavOmkar2112@users.noreply.github.com>
2021-07-14 22:38:10 +0530

    Add files via upload

---
+++ Pattern2.c
@@ -0,0 +1,33 @@
+// Input : 5
+// Output :  *  #  *  #  *  #  *  #  *  #
+
+// Input : 9
+// Output : *  #  *  #  *  #  *  #  *  #  *  #  *  #  *  #  *  #
+
+#include<stdio.h>
+
+typedef unsigned int  ABC;
+
+void Pattern(ABC iNo)
+{
+	int iCnt = 0;
+	
+	for(iCnt=1 ; iCnt<=iNo ; iCnt++)
+	{
+		printf("*\t#\t");
+	}
+	printf("\n");
+}
+
+
+int main()
+{
+	ABC iValue = 0;
+	
+	printf("Enter number");
+	scanf("%u",&iValue);
+	
+	Pattern(iValue);
+	
+	return 0;
+}
