abe1e59dae0cd09cf8099525a4252a2a209d8ff2
JadhavOmkar2112 <86395951+JadhavOmkar2112@users.noreply.github.com>
2021-07-17 23:48:53 +0530

    Add files via upload

---
+++ Pattern5.c
@@ -0,0 +1,40 @@
+// Input : 4   3
+// Output :
+/*
+	*  *  *
+	*  *  *
+	*  *  *
+	*  *  *
+*/
+
+#include<stdio.h>
+
+typedef unsigned int  ABC;
+
+void Pattern(ABC iRow,ABC iCol)
+{
+	int i = 0,j = 0;
+	for(i = 1 ; i<=iRow ; i++)
+	{
+		for(j=1 ; j<=iCol ; j++)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	ABC iValue1 = 0,iValue2 = 0;
+	
+	printf("Enter number of rows");
+	scanf("%u",&iValue1);
+	
+	printf("Enter number of columns");
+	scanf("%u",&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	
+	return 0;
+}
