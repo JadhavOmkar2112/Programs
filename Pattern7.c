57ed846def0f466381bec2354dffc10abe387e51
JadhavOmkar2112 <86395951+JadhavOmkar2112@users.noreply.github.com>
2021-07-20 21:33:10 +0530

    Add files via upload

---
+++ Pattern7.c
@@ -0,0 +1,49 @@
+/*
+	Input : iRow=3  iCol=3
+	Output : 
+	
+	1	2	3
+	*	*	*
+	1	2	3
+	
+*/
+
+#include<stdio.h>
+
+typedef unsigned int  ABC;
+
+void Pattern(ABC iRow,ABC iCol)
+{
+	int i = 0;
+	int j = 0;
+	for(i=1 ; i<=iRow ; i++)
+	{
+		for(j=1 ; j<=iCol ; j++)
+		{
+			if((i%2) == 0)
+			{
+				printf("*\t");
+			}
+			else
+			{
+				printf("%d\t",j);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	ABC iValue1 = 0,iValue2 = 0;
+	
+	printf("Enter number of rows");
+	scanf("%u",&iValue1);
+	
+	printf("Enter number of columns");
+	scanf("%u",&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	
+	return 0;
+}
