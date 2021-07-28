e2eca3c04b4dbbe690d8723e1a1d8d87122454a1
JadhavOmkar2112 <86395951+JadhavOmkar2112@users.noreply.github.com>
2021-07-28 23:01:18 +0530

    Add files via upload

---
+++ Trangle_Pattern.c
@@ -0,0 +1,51 @@
+/*
+	Input : 4	4
+	Output : 
+	
+	*
+	*	*
+	*	*	*
+	*	*	*	*
+	
+	
+*/
+
+#include<stdio.h>
+
+typedef unsigned int  ABC;
+
+void Pattern(ABC iRow,ABC iCol)
+{
+	int i = 0;
+	int j = 0;
+	if(iRow != iCol)
+	{
+		return;
+	}
+	for(i=1 ; i<=iRow ; i++)
+	{
+		for(j=1 ; j<=iCol ; j++)
+		{
+			if( i>=j )
+			{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	ABC iValue1 = 0,iValue2 = 0;
+	
+	printf("Enter number of rows");
+	scanf("%u",&iValue1);
+	
+	printf("Enter number of columns");
+	scanf("%u",&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	
+	return 0;
+}
