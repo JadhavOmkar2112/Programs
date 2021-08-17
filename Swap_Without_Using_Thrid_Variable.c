0fbdaecbec072406c9b098a65ebe7d1f7b3b08e3
JadhavOmkar2112 <86395951+JadhavOmkar2112@users.noreply.github.com>
2021-08-18 00:49:08 +0530

    Add files via upload

---
+++ Swap_Without_Using_Thrid_Variable.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void Swap(int *No1,int *No2)
+{	
+	(*No1) = (*No1) + (*No2);
+	
+	(*No2) = (*No1) - (*No2);
+	
+	(*No1) = (*No1) - (*No2);
+	
+}
+
+int main()
+{
+	int iNo1 = 0, iNo2 = 0;
+	
+	printf("Enter 1st number : ");
+	scanf("%d",&iNo1);
+	
+	printf("Enter 2nd number : ");
+	scanf("%d",&iNo2);
+	
+	printf("Before Swaping \n iNo1 : %d \n iNo2 : %d\n",iNo1,iNo2);
+	
+	Swap(&iNo1,&iNo2);
+	
+	printf("After Swaping \n iNo1 : %d \n iNo2 : %d",iNo1,iNo2);
+	
+	return 0;
+}
