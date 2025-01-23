99634fcff93cae68aa2dbc90874e051a4f4fb8a6
Omkar Jadhav <omkarjadhav9163@gmail.com>
2025-01-23 10:55:39 +0530

    Y

---
+++ Program2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Jay Shree Ram...\n");
+	
+	return 0;
+}
+
+// ./Myexe -> OS -> main() -> end of main -> return 0 to OS (it indicates success)
+
+// return from main to opertaing system
+// return 0	successful termination
+// return 1	abortive termination
+// return -1	erronious termination
+
